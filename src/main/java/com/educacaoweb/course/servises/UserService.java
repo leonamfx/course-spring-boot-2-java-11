2bf476db916ec5b510cb36a9580706666034f070
Leonam Lima <leonamfx@gmail.com>
2019-09-17 19:23:55 -0300

    UserDTO, findById, findAll

--- src/main/java/com/educacaoweb/course/servises/UserService.java
+++ src/main/java/com/educacaoweb/course/servises/UserService.java
@@ -2,6 +2,7 @@ package com.educacaoweb.course.servises;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -10,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.educacaoweb.course.dto.UserDTO;
 import com.educacaoweb.course.entities.User;
 import com.educacaoweb.course.repositories.UserRepository;
 import com.educacaoweb.course.servises.exceptions.DatabaseException;
@@ -22,13 +24,15 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	public List<User> findAll() {
-		return repository.findAll();
+	public List<UserDTO> findAll() {
+		List<User> list = repository.findAll();
+		return list.stream().map(e -> new UserDTO(e)).collect(Collectors.toList());
 	}
 	
-	public User findById(Long id) {
+	public UserDTO findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new UserDTO(entity);
 	}
 	
 	public User insert(User obj) {
