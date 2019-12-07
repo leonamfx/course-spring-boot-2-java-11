46b7245e076a98e54aa0d12a513ac6d605cb0e3d
LeonamLima <leonamfx@gmail.com>
2019-12-07 17:01:55 -0300

    Forgot Password, Logger

--- src/main/java/com/educacaoweb/course/resourses/AuthResource.java
+++ src/main/java/com/educacaoweb/course/resourses/AuthResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educacaoweb.course.dto.CredentialsDTO;
+import com.educacaoweb.course.dto.EmailDTO;
 import com.educacaoweb.course.dto.TokenDTO;
 import com.educacaoweb.course.servises.AuthService;
 
@@ -20,17 +21,24 @@ public class AuthResource {
 	private AuthService service;
 
 
-	@PostMapping("/login")
+	@PostMapping(value="/login")
 	public ResponseEntity<TokenDTO> login(@RequestBody CredentialsDTO dto){
 		TokenDTO tokenDto= service.authenticate(dto);
 
 		return ResponseEntity.ok().body(tokenDto);
 	}
 	
-	@PostMapping("/refresh")
+	@PostMapping(value="/refresh")
 	public ResponseEntity<TokenDTO> refresh(){
 		TokenDTO tokenDto= service.refreshToken();
 
 		return ResponseEntity.ok().body(tokenDto);
+	}
+	
+	@PostMapping(value="/forgot")
+	public ResponseEntity<Void> forgot(@RequestBody EmailDTO dto){
+	   service.sendNewPassword(dto.getEmail());
+
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/educacaoweb/course/servises/AuthService.java
+++ src/main/java/com/educacaoweb/course/servises/AuthService.java
@@ -1,11 +1,16 @@
 package com.educacaoweb.course.servises;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,16 +22,26 @@ import com.educacaoweb.course.repositories.UserRepository;
 import com.educacaoweb.course.security.JWTUtil;
 import com.educacaoweb.course.services.exceptions.JWTAuthenticationException;
 import com.educacaoweb.course.services.exceptions.JWTAuthorizationException;
+import com.educacaoweb.course.servises.exceptions.ResourceNotFoundException;
+
 
 
 
 @Service
 public class AuthService {
+	
+	private static final Logger LOG= LoggerFactory.getLogger(AuthService.class);
+
+	
+	
 	@Autowired
 	private AuthenticationManager authenicationManager;
 
 	@Autowired
 	private  JWTUtil jwtUtil;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncode;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -74,5 +89,42 @@ public class AuthService {
 		User user= authenticated();
 		return new TokenDTO(user.getEmail(),jwtUtil.generateToken(user.getEmail()));
 
+	}
+	
+	@Transactional
+	public void sendNewPassword(String email) {
+		User user= userRepository.findByEmail(email);
+		if( user == null) {
+			throw new ResourceNotFoundException("Email not found");
+		}
+
+	    String newPass= newPassword();
+	    user.setPassword(passwordEncode.encode(newPass));
+	    userRepository.save(user);
+
+	    LOG.info("New Password "+newPass);
+
+
+	}
+
+	private String newPassword() {
+		char[] vect= new char[10];
+		for(int i=0;i<10;i++) {
+			vect[i]= randomChar();
+		}
+
+		return new String(vect);
+	}
+	private char randomChar() {
+		Random rand = new Random();
+		int opt = rand.nextInt(3);
+		if(opt==0) {
+
+			return (char) (rand.nextInt(10) + 48);
+		}else if(opt==1) {
+			return (char) (rand.nextInt(26) + 65);
+		}else {
+			return (char) (rand.nextInt(26) + 97);
+		}
 	}
 }
