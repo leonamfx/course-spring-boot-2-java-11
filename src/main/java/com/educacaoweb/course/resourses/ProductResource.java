628c93896067c0293cd9190dd89c6a3941afed34
LeonamLima <leonamfx@gmail.com>
2019-12-07 10:54:38 -0300

    Product Update

--- src/main/java/com/educacaoweb/course/resourses/ProductResource.java
+++ src/main/java/com/educacaoweb/course/resourses/ProductResource.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.educacaoweb.course.dto.ProductCategoriesDTO;
 import com.educacaoweb.course.dto.ProductDTO;
 import com.educacaoweb.course.entities.Product;
 import com.educacaoweb.course.servises.ProductService;
@@ -30,6 +34,13 @@ public class ProductResource {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto){
+		ProductDTO newdto = service.update(id, dto);
+		return ResponseEntity.ok().body(newdto);
 	}
 	
 }
--- src/main/java/com/educacaoweb/course/servises/ProductService.java
+++ src/main/java/com/educacaoweb/course/servises/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import com.educacaoweb.course.entities.Category;
 import com.educacaoweb.course.entities.Product;
 import com.educacaoweb.course.repositories.CategoryRepository;
 import com.educacaoweb.course.repositories.ProductRepository;
+import com.educacaoweb.course.servises.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -44,6 +46,29 @@ public class ProductService {
 		setProductCategories(entity, dto.getCategories());
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO  dto) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, dto); 
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());	
+		entity.setImgUrl(dto.getImgUrl());
+
+		if(dto.getCategories()!=null && dto.getCategories().size()>0) {
+			setProductCategories(entity, dto.getCategories());
+		}
 	}
 
 	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
