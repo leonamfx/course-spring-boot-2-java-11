6a85c31dc6e78030416092689a6cb7644b6afe63
LeonamLima <leonamfx@gmail.com>
2019-12-07 10:46:52 -0300

    Product Insert

--- src/main/java/com/educacaoweb/course/servises/ProductService.java
+++ src/main/java/com/educacaoweb/course/servises/ProductService.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educacaoweb.course.dto.CategoryDTO;
+import com.educacaoweb.course.dto.ProductCategoriesDTO;
 import com.educacaoweb.course.dto.ProductDTO;
+import com.educacaoweb.course.entities.Category;
 import com.educacaoweb.course.entities.Product;
+import com.educacaoweb.course.repositories.CategoryRepository;
 import com.educacaoweb.course.repositories.ProductRepository;
 
 @Service
@@ -17,6 +23,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	public List<ProductDTO> findAll() {
 		List<Product> list = repository.findAll();
@@ -27,5 +36,30 @@ public class ProductService {
 		Optional<Product> obj = repository.findById(id);
 		Product entity = obj.get();
 		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO  dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+
+		for(CategoryDTO dto: categories) {
+				Category category = categoryRepository.getOne(dto.getId());
+				entity.getCategories().add(category);
+		}
+	}
+	
+	public CategoryRepository getCategoryRepository() {
+		return categoryRepository;
+	}
+
+	public void setCategoryRepository(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
 	}
 }
