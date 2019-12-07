f3bb8809151c26c9098caefc0c66829c6a9d44eb
LeonamLima <leonamfx@gmail.com>
2019-12-07 16:40:26 -0300

    User FindById and Update

--- src/main/java/com/educacaoweb/course/entities/User.java
+++ src/main/java/com/educacaoweb/course/entities/User.java
@@ -168,6 +168,15 @@ public class User implements UserDetails {
 		return false;
 	}
 
+	public boolean hasRole(String roleName) {
+
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 
 
--- src/main/java/com/educacaoweb/course/resourses/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educacaoweb/course/resourses/exceptions/ResourceExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educacaoweb.course.services.exceptions.JWTAuthorizationException;
 import com.educacaoweb.course.servises.exceptions.DatabaseException;
 import com.educacaoweb.course.servises.exceptions.ResourceNotFoundException;
 
@@ -52,6 +53,15 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> jwtAuthentication(JWTAuthenticationException e, HttpServletRequest request){
 		String error ="Authentication error";
 		HttpStatus status= HttpStatus.UNAUTHORIZED;
+		StandardError err= new StandardError(Instant.now(), status.value(),error,e.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+
+	}
+	@ExceptionHandler(JWTAuthorizationException.class)
+	public ResponseEntity<StandardError> jwtAuthorization(JWTAuthorizationException e, HttpServletRequest request){
+		String error ="Authentication error";
+		HttpStatus status= HttpStatus.FORBIDDEN;
 		StandardError err= new StandardError(Instant.now(), status.value(),error,e.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(status).body(err);
