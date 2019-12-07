f6d9c325d242a8e0716692c4dc53f767cbc5d552
LeonamLima <leonamfx@gmail.com>
2019-12-07 11:30:24 -0300

    Custom User Update Validation

---
+++ src/main/java/com/educacaoweb/course/services/validation/UserUpdateValidator.java
@@ -0,0 +1,56 @@
+package com.educacaoweb.course.services.validation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.educacaoweb.course.dto.UserDTO;
+import com.educacaoweb.course.entities.User;
+import com.educacaoweb.course.repositories.UserRepository;
+import com.educacaoweb.course.resourses.exceptions.FieldMessage;
+
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void initialize(UserUpdateValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserDTO dto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map =
+			(Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id"));
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		User user = userRepository.findByEmail(dto.getEmail());
+
+		if(user != null && !user.getId().equals(uriId)){
+            list.add(new FieldMessage("email", "email already taken"));
+        }
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();//se a lista tiver vazia significa que nao tem erro
+	}
+}
