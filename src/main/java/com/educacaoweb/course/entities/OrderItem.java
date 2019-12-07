840e806bf2cc78791de9ee4824acfa63cdf5eca0
LeonamLima <leonamfx@gmail.com>
2019-12-07 10:32:33 -0300

    Refactor no more Jackson in entities

--- src/main/java/com/educacaoweb/course/entities/OrderItem.java
+++ src/main/java/com/educacaoweb/course/entities/OrderItem.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.educacaoweb.course.entities.pk.OrderItemPk;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name="tb_order_item")
@@ -31,7 +31,6 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
-	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
