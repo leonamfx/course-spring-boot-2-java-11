7431ddb4ff3aca080701dc34c82319e3766becbf
LeonamLima <leonamfx@gmail.com>
2019-12-08 21:39:24 -0300

    Update Order

--- src/main/java/com/educacaoweb/course/resourses/OrderResource.java
+++ src/main/java/com/educacaoweb/course/resourses/OrderResource.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,4 +68,11 @@ public class OrderResource {
 		return ResponseEntity.created(uri).body(orderDTO);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value="/{id}")
+	public ResponseEntity<OrderDTO> update(@PathVariable Long id,@RequestBody OrderDTO dto){
+		dto= service.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+
+	}
 }
